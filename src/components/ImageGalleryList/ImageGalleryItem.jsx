3d1abc6f93959a3ae02a28040d22b0510dddd581
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-26 08:26:24 +0300

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryList/ImageGalleryItem.jsx
@@ -0,0 +1,18 @@
+const ImageGalleryItem = ({
+    webformatURL,
+    tags,
+    largeImageURL,
+    modalImage,
+}) => {
+    return (
+        <li className={css.ImageGalleryItem}>
+            <img
+                className={css.ImageGalleryItem_image}
+                src={webformatURL}
+                alt={tags}
+                data-url={largeImageURL}
+                onClick={() => modalImage(largeImageURL)}
+            />
+        </li>
+    );
+};
